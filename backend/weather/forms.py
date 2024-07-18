bb67cdf99499c94abc33e836008a71023c33d5fd
Yaroslav <105550736+iamzanuda@users.noreply.github.com>
2024-07-18 17:47:15 +0300

    Нстроил отображение help_text. Дополнил README. Удалил лишние коментарии в файлах.

--- backend/weather/forms.py
+++ backend/weather/forms.py
@@ -1,7 +1,17 @@
 from django import forms
+from django.core.validators import RegexValidator
+
+# Валидатор, разрешающий только буквы и пробелы
+alpha_space_validator = RegexValidator(
+    regex=r'^[a-zA-Z\s]+$',
+    message='Это что такое? Давай-ка попробуем ввести название города буковками.'
+)
 
 class CityForm(forms.Form):
     city = forms.CharField(
         max_length=100,
-        widget=forms.TextInput(attrs={'placeholder': 'В городе...'})
+        validators=[alpha_space_validator],
+        help_text='Ведите название города на английском языке.',
+        widget=forms.TextInput(
+            attrs={'placeholder': 'В городе...'})
     )
--- backend/weather/tests.py
+++ backend/weather/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .forms import CityForm
 
-# Create your tests here.
+class CityFormTest(TestCase):
+    """
+    Тесты для формы ввода города.
+    """
+
+    def test_city_form_valid(self):
+        """
+        Проверка валидации формы с верными данными города.
+        """
+
+        form_data = {'city': 'Moscow'}
+        form = CityForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_city_form_invalid(self):
+        """
+        Проверка валидации формы с неверными данными города.
+        """
+
+        form_data = {'city': ''}
+        form = CityForm(data=form_data)
+        self.assertFalse(form.is_valid())
